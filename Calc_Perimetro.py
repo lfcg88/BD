36a1d6967552256515ea5242db9ec2a666be32ec
lfcg88 <33296318+lfcg88@users.noreply.github.com>
2017-11-01 18:32:56 -0200

    Delete Calc_Perimetro.py

--- Calc_Perimetro.py
+++
@@ -1,8 +0,0 @@
-val_quad = input("Digite o valor correspondente ao lado de um quadrado: ")
-print()
-val_quad_conv = int(val_quad)
-
-perimetro = val_quad_conv * 4
-area = val_quad_conv ** 2
-
-print("perímetro: ",perimetro, " - área: ", area)
