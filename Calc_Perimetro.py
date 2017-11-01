7c880a12c9676d4f83830f339f7449d0085bfb3d
lfcg88 <33296318+lfcg88@users.noreply.github.com>
2017-11-01 18:30:45 -0200

    Create Calc_Perimetro.py

---
+++ Calc_Perimetro.py
@@ -0,0 +1,8 @@
+val_quad = input("Digite o valor correspondente ao lado de um quadrado: ")
+print()
+val_quad_conv = int(val_quad)
+
+perimetro = val_quad_conv * 4
+area = val_quad_conv ** 2
+
+print("perímetro: ",perimetro, " - área: ", area)
